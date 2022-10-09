21b3d2e121243bf195e29c0c2cce1cde5177c235
jb corbin <jamesbcorbin@gmail.com>
2022-10-09 12:00:27 -0500

    added info to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,6 +10,7 @@ const About = () => {
     <div style={{height: '100vh'}}>
       <h1>About Page</h1>
       <p>ETrealty is here to help you find your new home, or sell your current home. We will provide you with the knowledge and skills you need for a succesfull real estate experience</p>
+      <p> We pride ourselves in offering the best hometown realtor experience in Bates County.</p>
     </div>
    </div>
   )
