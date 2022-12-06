65cb0a73194ffa312450551d89e7fb653f9ad339
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-12-05 20:11:14 -0600

    update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,7 @@ const About = () => {
     <Head>
       <title>About</title>
     </Head>
-    <div style={{height: '100vh'}}>
+    <div style={{height: '100vh', textAlign: 'center'}}>
       <h1>About Page</h1>
       <p>ETrealty is here to help you find your new home, or sell your current home. We will provide you with the knowledge and skills you need for a succesfull real estate experience</p>
       <p> We pride ourselves in offering the best hometown realtor experience in Bates County.</p>
