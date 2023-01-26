8b7bdcd420435cdc3f888adea491ecb548452741
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-01-26 06:49:30 -0600

    added switch case, not finished

---
+++ components/DescriptionBox.tsx
@@ -0,0 +1,37 @@
+import { Md7K } from "react-icons/md";
+
+export default function DescriptionBox(id: string, state: string, address: string, bed: number, bath: number, squarefootage: number, lotsize: number) {
+    const pID = parseInt(id);
+    switch(pID) {
+        case 0: 
+            
+            break;
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 6:
+            
+            break;
+        case 7:
+            
+            break;
+        case 8:
+
+            break;
+        case 9:
+
+            break;
+    }
+}
