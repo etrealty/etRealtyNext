ab159efbadc33b3785b854f623c2a4b76b98b45f
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-02-18 11:35:58 -0600

    added realtor photos

--- pages/Contact/index.tsx
+++ pages/Contact/index.tsx
@@ -28,23 +28,32 @@ const Contact = () => {
         <h2 style={{textAlign: 'center'}}><u>Realtors</u></h2>
         <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
             <div>
-                <p>Ella Tindle</p>
-                <p>816-225-4500</p>
-                <p>ellastark@sbcglobal.net</p>
+                <img style={{borderRadius: '90px'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740975/1homeland_reality-8_rhxnde.jpg" alt="E Tindle" />
+                <div>
+                    <p>Ella Tindle</p>
+                    <p>816-225-4500</p>
+                    <p>ellastark@sbcglobal.net</p>
+                </div>
             </div>
             <div>
-                <p>Ben Johnson</p>
-                <p>417-298-7678</p>
-                <p>benjaminljohnson@yahoo.com</p>
+                <img  style={{borderRadius: '90px'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740974/1homeland_reality-49_oftiri.jpg" alt="B Johnson" />
+                <div>
+                    <p>Ben Johnson</p>
+                    <p>417-298-7678</p>
+                    <p>benjaminljohnson@yahoo.com</p>
+                </div>
             </div>
             <div>
-                <p>James W. Langley</p>
-                <p>816-590-5259</p>
-                <p>james.w.langley@gmail.com</p>
+                <img style={{borderRadius: '90px'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740978/1homeland_reality-50_b5rbxx.jpg" alt="J Langley" />
+                <div>
+                    <p>James W. Langley</p>
+                    <p>816-590-5259</p>
+                    <p>james.w.langley@gmail.com</p>
+                </div>
             </div>
         </div>
       </div>
-
+ 
 
 
     </div>
