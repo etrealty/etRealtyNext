3253bb01df721433e871e29e05a9a249a4a9a668
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-02-25 10:37:36 -0600

    added border to contact page info

--- pages/Contact/index.tsx
+++ pages/Contact/index.tsx
@@ -28,7 +28,7 @@ const Contact = () => {
         <h2 style={{textAlign: 'center'}}><u>Realtors</u></h2>
         <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
             <div>
-                <img style={{borderRadius: '90px'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740975/1homeland_reality-8_rhxnde.jpg" alt="E Tindle" />
+                <img style={{borderRadius: '90px', border: '2px black'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740975/1homeland_reality-8_rhxnde.jpg" alt="E Tindle" />
                 <div>
                     <p>Ella Tindle</p>
                     <p>816-225-4500</p>
@@ -36,7 +36,7 @@ const Contact = () => {
                 </div>
             </div>
             <div>
-                <img  style={{borderRadius: '90px'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740974/1homeland_reality-49_oftiri.jpg" alt="B Johnson" />
+                <img  style={{borderRadius: '90px', border: '2px black'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740974/1homeland_reality-49_oftiri.jpg" alt="B Johnson" />
                 <div>
                     <p>Ben Johnson</p>
                     <p>417-298-7678</p>
@@ -44,7 +44,7 @@ const Contact = () => {
                 </div>
             </div>
             <div>
-                <img style={{borderRadius: '90px'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740978/1homeland_reality-50_b5rbxx.jpg" alt="J Langley" />
+                <img style={{borderRadius: '90px', border: '2px black'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740978/1homeland_reality-50_b5rbxx.jpg" alt="J Langley" />
                 <div>
                     <p>James W. Langley</p>
                     <p>816-590-5259</p>
