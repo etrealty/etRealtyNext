29df9893a9bc1bb2ec3ce578b613760e5691ecca
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-02-26 14:59:26 -0600

    added background to contact card

--- pages/Contact/index.tsx
+++ pages/Contact/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head"
 
+import styles from '../../styles/Contact.module.css'
+
 const Contact = () => {
   
   
@@ -27,24 +29,24 @@ const Contact = () => {
         <br/>
         <h2 style={{textAlign: 'center'}}><u>Realtors</u></h2>
         <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
-            <div>
-                <img style={{borderRadius: '90px', border: '2px black'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740975/1homeland_reality-8_rhxnde.jpg" alt="E Tindle" />
+            <div className={styles.ContactCard}>
+                <img style={{borderRadius: '90px'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740975/1homeland_reality-8_rhxnde.jpg" alt="E Tindle" />
                 <div>
                     <p>Ella Tindle</p>
                     <p>816-225-4500</p>
                     <p>ellastark@sbcglobal.net</p>
                 </div>
             </div>
-            <div>
-                <img  style={{borderRadius: '90px', border: '2px black'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740974/1homeland_reality-49_oftiri.jpg" alt="B Johnson" />
+            <div className={styles.ContactCard}>
+                <img  style={{borderRadius: '90px'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740974/1homeland_reality-49_oftiri.jpg" alt="B Johnson" />
                 <div>
                     <p>Ben Johnson</p>
                     <p>417-298-7678</p>
                     <p>benjaminljohnson@yahoo.com</p>
                 </div>
             </div>
-            <div>
-                <img style={{borderRadius: '90px', border: '2px black'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740978/1homeland_reality-50_b5rbxx.jpg" alt="J Langley" />
+            <div className={styles.ContactCard}>
+                <img style={{borderRadius: '90px'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740978/1homeland_reality-50_b5rbxx.jpg" alt="J Langley" />
                 <div>
                     <p>James W. Langley</p>
                     <p>816-590-5259</p>
