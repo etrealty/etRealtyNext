85ba929d1f261bd7e13570b745567200e5da84d9
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-02-27 07:57:35 -0600

    fixed Card info

--- pages/Contact/index.tsx
+++ pages/Contact/index.tsx
@@ -3,9 +3,7 @@ import Head from "next/head"
 import styles from '../../styles/Contact.module.css'
 
 const Contact = () => {
-  
-  
-  
+    
   
   return (
     <div>
@@ -31,7 +29,7 @@ const Contact = () => {
         <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
             <div className={styles.ContactCard}>
                 <img style={{borderRadius: '90px'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740975/1homeland_reality-8_rhxnde.jpg" alt="E Tindle" />
-                <div>
+                <div className={styles.CCinfo}>
                     <p>Ella Tindle</p>
                     <p>816-225-4500</p>
                     <p>ellastark@sbcglobal.net</p>
@@ -39,7 +37,7 @@ const Contact = () => {
             </div>
             <div className={styles.ContactCard}>
                 <img  style={{borderRadius: '90px'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740974/1homeland_reality-49_oftiri.jpg" alt="B Johnson" />
-                <div>
+                <div className={styles.CCinfo}>
                     <p>Ben Johnson</p>
                     <p>417-298-7678</p>
                     <p>benjaminljohnson@yahoo.com</p>
@@ -47,7 +45,7 @@ const Contact = () => {
             </div>
             <div className={styles.ContactCard}>
                 <img style={{borderRadius: '90px'}} src="https://res.cloudinary.com/dq6yyefdy/image/upload/w_150,h_150,c_fill,g_face,r_max/v1676740978/1homeland_reality-50_b5rbxx.jpg" alt="J Langley" />
-                <div>
+                <div className={styles.CCinfo}>
                     <p>James W. Langley</p>
                     <p>816-590-5259</p>
                     <p>james.w.langley@gmail.com</p>
