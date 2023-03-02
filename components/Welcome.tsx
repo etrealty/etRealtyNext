7cfe15470600cae91cc5972084eb44011b3429b4
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-03-02 13:32:59 -0600

    updated index page

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -2,12 +2,15 @@ const Welcome = () => {
 
     return (
         <div style={{background: '#DDE3DD'}}>
-            <h1 style={{textAlign: 'center'}}>
-                Welcome to Homeland Realty!
-            </h1>
-            <h2 style={{textAlign: 'center'}}>
+            <div style={{boxShadow: 'grey 8px 8px 2px 2px' ,margin: '40px',border: 'solid black 2px', borderRadius: '5px',background: '#8EA38F', color: '#DDE3DD'}}>
+                <h1 style={{textAlign: 'center'}}>
+                    Homeland Realty
+                </h1>
+                <p style={{textAlign: 'center'}}><i>A new home, a fresh start.</i></p>
+            </div> 
+            <h3 style={{textAlign: 'center'}}>
                 With 15+ years of experience as a Realtor, Ella Tindle and her know team know real estate.
-            </h2>
+            </h3>
             <h3 style={{textAlign: 'center'}}>
                 Whether it's selling your family home or buying your dream home. We have the knowledge to get you the best price. With the least amount of stress.
             </h3>
